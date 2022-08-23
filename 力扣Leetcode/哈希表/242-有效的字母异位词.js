7cab0e770f2400bb3413b43149fc42088cdfaae0
leishao06 <987313405@qq.com>
2022-08-24 00:27:36 +0800

    242-二刷思路

--- 力扣Leetcode/哈希表/242-有效的字母异位词.js
+++ 力扣Leetcode/哈希表/242-有效的字母异位词.js
@@ -8,16 +8,56 @@
  * 遍历完之后，都没有停止，说明对应字母数量都相同，最后return true
  */
 
-var isAnagram = function(s, t) {
+var isAnagram = function (s, t) {
   if (s.length != t.length) return false;
   const record = new Array(26).fill(0);
   const base = "a".charCodeAt();
   for (let v of s) {
-      record[v.charCodeAt() - base]++;
+    record[v.charCodeAt() - base]++;
   }
   for (let v of t) {
-      if (record[v.charCodeAt() - base] === 0) return false;
-      record[v.charCodeAt() - base]--;
+    if (record[v.charCodeAt() - base] === 0) return false;
+    record[v.charCodeAt() - base]--;
+  }
+  return true;
+};
+
+/**
+ * 二刷思路：
+ * 如果两个字符串长度不相等，那么对应字母更不可能全相等，直接return false
+ * 如果长度相当，遍历一遍，把两个数组各字母出现次数，分别存在map1和map2中，
+ * 如果两个map的size不相等，说明对应字母个数都不同，返回false，
+ * 否则遍历map1的key即字母，依次比较对应出现次数，如果不相等，返回false
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+  const map1 = new Map();
+  const map2 = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    if (map1.has(s[i])) {
+      map1.set(s[i], map1.get(s[i]) + 1);
+    } else {
+      map1.set(s[i], 1);
+    }
+
+    if (map2.has(t[i])) {
+      map2.set(t[i], map2.get(t[i]) + 1);
+    } else {
+      map2.set(t[i], 1);
+    }
+  }
+
+  if (map1.size !== map2.size) {
+    return false;
+  }
+
+  for (let k of map1.keys()) {
+    if (map1.get(k) !== map2.get(k)) {
+      return false;
+    }
   }
   return true;
 };
