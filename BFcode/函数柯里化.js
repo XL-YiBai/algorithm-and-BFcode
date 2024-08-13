e535b96a213d747fce8d0508164603db70fe75a6
XL-YiBai <lei.xia@emqx.io>
2024-08-14 00:04:31 +0800

    improve: curry

--- BFcode/函数柯里化.js
+++ BFcode/函数柯里化.js
@@ -1,27 +1,28 @@
 const curry = function (fn) {
-  fnLen = fn.length; // 传入的函数接受参数的个数
-  let args = []; // 使用闭包，保存每次函数传入的参数
+  fnLen = fn.length // 传入的函数接受参数的个数
+  let args = [] // 使用闭包，保存每次函数传入的参数
 
   function calc(...newArgs) {
-    args = [...args, ...newArgs]; // 把新的参数保存到args中
+    args = [...args, ...newArgs] // 把新的参数保存到args中
     // 如果参数已经接收完毕，就返回函数fn的执行结果
-    if (args.length == fnLen) {
-      return fn.apply(this, [...args]);
+    if (args.length >= fnLen) {
+      // return fn(...args)
+      return fn.apply(this, [...args])
     } else {
       //否则继续返回当前函数
-      return calc;
+      return calc
     }
   }
 
-  return calc();
-};
+  return calc()
+}
 
 // 功能测试
 function add(a, b, c) {
   // console.log(this); // window
-  return a + b + c;
+  return a + b + c
 }
 
-const curryAdd = curry(add);
-const res = curryAdd(10)(20)(30); // 60
-console.log(res);
+const curryAdd = curry(add)
+const res = curryAdd(10)(20)(30) // 60
+console.log(res)
